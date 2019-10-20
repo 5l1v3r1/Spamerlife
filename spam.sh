6b43a5dcb068516adfc1eb4773b80a807c7c82bf
The Sixty Nine <52529306+thesixtynine@users.noreply.github.com>
2019-10-20 18:25:16 +0700

    Create spam.sh

---
+++ spam.sh
@@ -0,0 +1,206 @@
+#!/usr/bin/shell
+# Life Of Programmer
+# Coded By The Sixty Nine
+
+clear
+reset
+
+echo "\033[0m"
+
+printf ".   ███████╗██████╗  █████╗ ███╗   ███╗███████╗██████╗
+    ██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔════╝██╔══██╗
+    ███████╗██████╔╝███████║██╔████╔██║█████╗  ██████╔╝
+    ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║██╔══╝  ██╔══██╗
+    ███████║██║     ██║  ██║██║ ╚═╝ ██║███████╗██║  ██║
+    ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
+" | lolcat
+
+echo
+sleep 1
+echo "\033[0m[\033[91;1m✺\033[0m] Various kinds of tools for spam \033[0m[\033[91;1m✺\033[0m]"
+sleep 1
+echo "\033[0m[\033[92;1m★\033[0m] Indonesia Security Lite \033[0m[\033[92;1m★\033[0m]"
+sleep 1
+echo "\033[0m[\033[93;1m●\033[0m] Spam Available \033[0m[\033[93;1m●\033[0m]"
+echo
+echo "\033[0m[\033[94;1m1\033[0m] \033[77;1mBukalapak"
+echo "\033[0m[\033[94;1m2\033[0m] \033[77;1mTelkomsel"
+echo "\033[0m[\033[94;1m3\033[0m] \033[77;1mGrab"
+echo "\033[0m[\033[94;1m4\033[0m] \033[77;1mTokopedia"
+echo "\033[0m[\033[94;1m5\033[0m] \033[77;1mCodaShop"
+echo "\033[0m[\033[94;1m6\033[0m] \033[77;1mSurveyon"
+echo "\033[0m[\033[94;1m7\033[0m] \033[77;1mCustom"
+echo "\033[0m[\033[94;1m0\033[0m] \033[77;1mExit"
+echo "\033[0m[\033[96;1mi\033[0m] \033[77;1mThis Tool Information"
+echo "\033[0m"
+
+read -p "[/] Select option : " opps
+
+if [ $opps = 1 ] || [ $opps = 1 ]
+then
+clear
+echo
+toilet -f slant "Bukalapak"
+echo
+echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mChat WhatsApp"
+echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo
+sleep 5
+clear
+cd $HOME/exstraspam/tools/
+php 1
+fi
+
+if
+[ $opps = 2 ] || [ $opps = 2 ]
+then
+clear
+echo
+toilet -f slant  "Telkomsel"
+echo
+echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mSMS"
+echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo
+sleep 5
+clear
+cd $HOME/exstraspam/tools/
+php 2.php
+fi
+
+if [ $opps = 3 ] || [ $opps = 3 ]
+then
+clear
+echo
+toilet -f slant "Grab"
+echo
+echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mTelepon"
+echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo
+sleep 5
+clear
+cd $HOME/exstraspam/tools/
+php 3.php
+fi
+
+
+if [ $opps = 4 ] || [ $opps = 4 ]
+then
+clear
+echo
+toilet -f slant "Tokopedia"
+echo
+echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mTelepon"
+echo "\033[31;1m[+] Limit: \033[0m\033[77;1m1 Kali"
+echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo
+sleep 5
+clear
+cd $HOME/exstraspam/tools/
+php 4.php
+fi
+
+if [ $opps = 5 ] || [ $opps = 5 ]
+then
+clear
+toilet -f slant "CodaShop"
+echo
+echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mSMS"
+echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo
+echo
+echo " \033[33;1m[#] COPY THE LISENSI: \033[77;1mJDJ5JDEwJC9YMWRvNC5jcmNOSUw2dGdUaUt2d08ucWEvWURWSFNCTXI3U21wdkdsR1FMcVBSTW1oNUZH"
+sleep 1
+echo " \033[32;1m[*] \033[77;1mPlease wait"
+echo
+sleep 5
+clear
+cd $HOME/exstraspam/tools/
+php 5.php
+fi
+
+if
+[ $opps = 6 ] || [ $opps = 6 ]
+then
+clear
+toilet -f slant "Surveyon"
+echo
+echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mEmail"
+echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo
+sleep 5
+cd $HOME/exstraspam/tools/
+php 6.php
+fi
+
+if
+[ $opps = 7 ] || [ $opps = 7 ]
+then
+clear
+toilet -f slant "SmsID"
+echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mSMS"
+echo "\033[31;1m[+] Limit: \033[0m\033[77;1m5"
+echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo
+echo
+echo "\033[33;1m[*] For more information visit: \033[77;1mhttps://amsitlab.github.io/smsid-java/"
+sleep 5
+smsid boom -y
+fi
+
+if
+[ $opps = i ] || [ $opps = I ]
+then
+clear
+
+toilet -f smslant  "   My  Info" --gay
+echo
+sleep 1
+echo "\033[0m[\033[31;1m~\033[0m] \033[77;1mFacebook: \033[0makun.official.016"
+sleep 1
+echo "\033[0m[\033[32;1m#\033[0m] \033[77;1mFInstagram: \033[0mdey_016"
+sleep 1
+echo "\033[0m[\033[33;1m*\033[0m] \033[77;1mFWhatsApp: \033[0m+62 8577-5433-901"
+sleep 1
+echo "\033[0m[\033[35;1m/\033[0m] \033[77;1mFGithub: \033[0mgithub.com/thesixtynine"
+sleep 1
+echo "\033[0m[\033[36;1m@\033[0m] \033[77;1mFWebsite: \033[0mwww.dey-oki.blogspot.com"
+
+sleep 1
+echo
+echo "\033[0m[\033[35;1m●\033[0m] \033[77;1mCoding: \033[91mBash"
+echo "\033[0m[\033[34;1m★\033[0m] \033[77;1mCoded by: \033[92mThe Sixty Nine"
+echo "\033[0m[\033[33;1m◈\033[0m] \033[77;1mMoodified: \033[93m Senja"
+echo "\033[0m[\033[32;1m❖\033[0m] \033[77;1mVersion: \033[94mv3.1.1"
+echo "\033[0m[\033[31;1m❂\033[0m] \033[77;1mTeam: \033[95mDexa Security Lite"
+
+sleep 1
+echo
+echo "\033[0m[\033[90;1m✦\033[0m] \033[77;1mPlease wait"
+sleep 10
+
+sh spam.sh
+fi
+
+if
+[ $opps = 0 ] || [ $opps = 0 ]
+then
+echo
+echo "\033[0m[\033[31;1m!\033[0m] \033[77;1mExit"
+sleep 1
+echo "\033[0m[\033[35;1m#\033[0m] \033[77;1mSee you again. may be useful"
+sleep 1
+fi
+echo
+echo
