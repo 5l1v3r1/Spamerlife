f169de47bbaa9041d558ac0391285f4ccbfcd1a0
The Sixty Nine <52529306+thesixtynine@users.noreply.github.com>
2019-10-27 10:53:43 +0700

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/shell
 # Life Of Programmer
-# Coded By The Sixty Nine
+# Coded By Nedi Senja
 
 clear
 reset
+sleep 1
+printf "
 
-echo "\033[0m"
-
-printf ".   ███████╗██████╗  █████╗ ███╗   ███╗███████╗██████╗
-    ██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔════╝██╔══██╗
-    ███████╗██████╔╝███████║██╔████╔██║█████╗  ██████╔╝
-    ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║██╔══╝  ██╔══██╗
-    ███████║██║     ██║  ██║██║ ╚═╝ ██║███████╗██║  ██║
-    ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
-" | lolcat
+\033[0;35m    ███████╗██████╗  █████╗ ███╗   ███╗███████╗██████╗
+\033[0;35m    ██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔════╝██╔══██╗
+\033[0;35m    ███████╗██████╔╝███████║██╔████╔██║█████╗  ██████╔╝
+\033[0;33m    ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║██╔══╝  ██╔══██╗
+\033[0;33m    ███████║██║     ██║  ██║██║ ╚═╝ ██║███████╗██║  ██║
+\033[0;33m    ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
+"
 
 echo
 sleep 1
 echo "\033[0m[\033[91;1m✺\033[0m] Various kinds of tools for spam \033[0m[\033[91;1m✺\033[0m]"
 sleep 1
-echo "\033[0m[\033[92;1m★\033[0m] Indonesia Security Lite \033[0m[\033[92;1m★\033[0m]"
+echo "\033[0m[\033[92;1m★\033[0m] Dexa Security Lite \033[0m[\033[92;1m★\033[0m]"
 sleep 1
 echo "\033[0m[\033[93;1m●\033[0m] Spam Available \033[0m[\033[93;1m●\033[0m]"
 echo
@@ -32,11 +32,11 @@ echo "\033[0m[\033[94;1m6\033[0m] \033[77;1mSurveyon"
 echo "\033[0m[\033[94;1m7\033[0m] \033[77;1mCustom"
 echo "\033[0m[\033[94;1m0\033[0m] \033[77;1mExit"
 echo "\033[0m[\033[96;1mi\033[0m] \033[77;1mThis Tool Information"
-echo "\033[0m"
+echo "\033[77;1m"
 
-read -p "[/] Select option : " opps
+read -p "[/] Select option : " ops
 
-if [ $opps = 1 ] || [ $opps = 1 ]
+if [ $ops = 1 ] || [ $ops = 01 ]
 then
 clear
 echo
@@ -45,7 +45,7 @@ echo
 echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mChat WhatsApp"
 echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
 echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
-echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo "\033[31;1m[+] Modified by: \033[0m\033[77;1mNedi Senja"
 echo
 sleep 5
 clear
@@ -54,16 +54,16 @@ php 1.php
 fi
 
 if
-[ $opps = 2 ] || [ $opps = 2 ]
+[ $ops = 2 ] || [ $ops = 02 ]
 then
 clear
 echo
 toilet -f slant  "Telkomsel"
 echo
-echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mSMS"
-echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
-echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
-echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo "\033[32;1m[+] Spam type: \033[0m\033[77;1mSMS"
+echo "\033[32;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[32;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[32;1m[+] Modified by: \033[0m\033[77;1mNedi Senja"
 echo
 sleep 5
 clear
@@ -71,16 +71,16 @@ cd $HOME/exstraspam/tools/
 php 2.php
 fi
 
-if [ $opps = 3 ] || [ $opps = 3 ]
+if [ $ops = 3 ] || [ $ops = 03 ]
 then
 clear
 echo
 toilet -f slant "Grab"
 echo
-echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mTelepon"
-echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
-echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
-echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo "\033[33;1m[+] Spam type: \033[0m\033[77;1mTelepon"
+echo "\033[33;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[33;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[33;1m[+] Modified by: \033[0m\033[77;1mNedi Senja"
 echo
 sleep 5
 clear
@@ -89,16 +89,16 @@ php 3.php
 fi
 
 
-if [ $opps = 4 ] || [ $opps = 4 ]
+if [ $ops = 4 ] || [ $ops = 04 ]
 then
 clear
 echo
 toilet -f slant "Tokopedia"
 echo
-echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mTelepon"
-echo "\033[31;1m[+] Limit: \033[0m\033[77;1m1 Kali"
-echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
-echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo "\033[34;1m[+] Spam type: \033[0m\033[77;1mTelepon"
+echo "\033[34;1m[+] Limit: \033[0m\033[77;1m1 Kali"
+echo "\033[34;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[34;1m[+] Modified by: \033[0m\033[77;1mNedi Senja"
 echo
 sleep 5
 clear
@@ -106,15 +106,15 @@ cd $HOME/exstraspam/tools/
 php 4.php
 fi
 
-if [ $opps = 5 ] || [ $opps = 5 ]
+if [ $ops = 5 ] || [ $ops = 05 ]
 then
 clear
 toilet -f slant "CodaShop"
 echo
-echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mSMS"
-echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
-echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
-echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo "\033[35;1m[+] Spam type: \033[0m\033[77;1mSMS"
+echo "\033[35;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[35;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[35;1m[+] Modified by: \033[0m\033[77;1mNedi Senja"
 echo
 echo
 echo " \033[33;1m[#] COPY THE LISENSI: \033[77;1mJDJ5JDEwJC9YMWRvNC5jcmNOSUw2dGdUaUt2d08ucWEvWURWSFNCTXI3U21wdkdsR1FMcVBSTW1oNUZH"
@@ -128,15 +128,15 @@ php 5.php
 fi
 
 if
-[ $opps = 6 ] || [ $opps = 6 ]
+[ $ops = 6 ] || [ $ops = 6 ]
 then
 clear
 toilet -f slant "Surveyon"
 echo
-echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mEmail"
-echo "\033[31;1m[+] Limit: \033[0m\033[77;1mUnlimited"
-echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
-echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo "\033[36;1m[+] Spam type: \033[0m\033[77;1mEmail"
+echo "\033[36;1m[+] Limit: \033[0m\033[77;1mUnlimited"
+echo "\033[36;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[36;1m[+] Modified by: \033[0m\033[77;1mNedi Senja"
 echo
 sleep 5
 cd $HOME/exstraspam/tools/
@@ -144,57 +144,62 @@ php 6.php
 fi
 
 if
-[ $opps = 7 ] || [ $opps = 7 ]
+[ $ops = 7 ] || [ $ops = 07 ]
 then
 clear
 toilet -f slant "SmsID"
-echo "\033[31;1m[+] Spam type: \033[0m\033[77;1mSMS"
-echo "\033[31;1m[+] Limit: \033[0m\033[77;1m5"
-echo "\033[31;1m[+] Coding: \033[0m\033[77;1mPHP"
-echo "\033[31;1m[+] Mood by: \033[0m\033[77;1mSenja"
+echo "\033[90;1m[+] Spam type: \033[0m\033[77;1mSMS"
+echo "\033[90;1m[+] Limit: \033[0m\033[77;1m5"
+echo "\033[90;1m[+] Coding: \033[0m\033[77;1mPHP"
+echo "\033[90;1m[+] Modified by: \033[0m\033[77;1mNedi Senja"
 echo
 echo
 echo "\033[33;1m[*] For more information visit: \033[77;1mhttps://amsitlab.github.io/smsid-java/"
 sleep 5
-php 3.php
+smsid boom -y
 fi
 
 if
-[ $opps = i ] || [ $opps = I ]
+[ $ops = i ] || [ $ops = I ]
 then
 clear
 
 toilet -f smslant  "   My  Info" --gay
 echo
+echo
 sleep 1
-echo "\033[0m[\033[31;1m~\033[0m] \033[77;1mFacebook: \033[0makun.official.016"
+echo "\033[0m[\033[31;1m~\033[0m] \033[77;1mFacebook: \033[0;36makun.official.016"
 sleep 1
-echo "\033[0m[\033[32;1m#\033[0m] \033[77;1mFInstagram: \033[0mdey_016"
+echo "\033[0m[\033[32;1m#\033[0m] \033[77;1mInstagram: \033[0;35mdey_016"
 sleep 1
-echo "\033[0m[\033[33;1m*\033[0m] \033[77;1mFWhatsApp: \033[0m+62 8577-5433-901"
+echo "\033[0m[\033[33;1m*\033[0m] \033[77;1mWhatsApp: \033[0;34m+62 8577-5433-901"
 sleep 1
-echo "\033[0m[\033[35;1m/\033[0m] \033[77;1mFGithub: \033[0mgithub.com/thesixtynine"
+echo "\033[0m[\033[34;1m&\033[0m] \033[77;1mEmail: \033[0;33md_q16x@outlook.co.id"
 sleep 1
-echo "\033[0m[\033[36;1m@\033[0m] \033[77;1mFWebsite: \033[0mwww.dey-oki.blogspot.com"
+echo "\033[0m[\033[35;1m/\033[0m] \033[77;1mGithub: \033[0;32mgithub.com/thesixtynine"
+sleep 1
+echo "\033[0m[\033[36;1m@\033[0m] \033[77;1mWebsite: \033[0;31mwww.dey-oki.blogspot.com"
 
 sleep 1
 echo
-echo "\033[0m[\033[35;1m●\033[0m] \033[77;1mCoding: \033[91mBash"
-echo "\033[0m[\033[34;1m★\033[0m] \033[77;1mCoded by: \033[92mThe Sixty Nine"
-echo "\033[0m[\033[33;1m◈\033[0m] \033[77;1mMoodified: \033[93m Senja"
-echo "\033[0m[\033[32;1m❖\033[0m] \033[77;1mVersion: \033[94mv3.1.1"
-echo "\033[0m[\033[31;1m❂\033[0m] \033[77;1mTeam: \033[95mDexa Security Lite"
+echo
+echo "\033[0m[\033[36;1m●\033[0m] \033[77;1mCoding: \033[91mBash"
+echo "\033[0m[\033[35;1m★\033[0m] \033[77;1mCoded by \033[92mNedi Senja"
+echo "\033[0m[\033[34;1m◈\033[0m] \033[77;1mGithub: \033[93mThe Sixty Nine"
+echo "\033[0m[\033[33;1m❖\033[0m] \033[77;1mVersion: \033[94mv3.1.1"
+echo "\033[0m[\033[32;1m❂\033[0m] \033[77;1mTeam: \033[95mDexa Security Lite"
+echo "\033[0m[\033[31;1m▣\033[0m] \033[77;1mUpgrade: \033[96m01 Feb 2019"
 
 sleep 1
 echo
-echo "\033[0m[\033[90;1m✦\033[0m] \033[77;1mPlease wait"
+echo "\033[0m[\033[90;1m✦\033[0m] \033[77;1mPlease wait ..."
 sleep 10
 
-sh spam.sh
+sh myspam.sh
 fi
 
 if
-[ $opps = 0 ] || [ $opps = 0 ]
+[ $ops = 0 ] || [ $ops = 00 ]
 then
 echo
 echo "\033[0m[\033[31;1m!\033[0m] \033[77;1mExit"
@@ -204,3 +209,4 @@ sleep 1
 fi
 echo
 echo
+exit 1
