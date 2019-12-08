4b6f9e584e9e2d3059a70685db724b4bb9ae468f
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2019-12-08 10:45:28 +0700

    Update and rename spamer.sh to spamerlife.sh

--- spamerlife.sh
+++ spamerlife.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/bash
 # Spamerlife
 # Coded by Senja
-# Github: github.com/thedarksec/Spamerlife
+# Github: github.com/stepbystepexe/Spamerlife
 
 checkroot() {
 
 if [[ "$(id -u)" -ne 0 ]]; then
-    printf "\e[1;77mPlease, run this program as root!\n\e[0m"
+    printf "\e[0m[\e[1;91m!\e[0m] \e[1;77mPlease, run this program as root!\n\n\e[0m"
     exit 1
-fi
-
-}
-
-checktor() {
-
-check=$(curl --socks5-hostname localhost:9050 -s https://www.google.com > /dev/null; echo $?)
-
-if [[ "$check" -gt 0 ]]; then
-    printf "\e[1;77mPlease, check your TOR Connection! Just type \"tor\" or \"service tor start\"\n\e[0m"
-    exit 1
-
 fi
 
 }
@@ -34,10 +22,10 @@ sleep 1
 
 dependencies() {
 
-command -v git > /dev/null 2>&1 || { echo >&2 "I require git but it's not installed. Install it. Aborting."; exit 1; }
 command -v bash > /dev/null 2>&1 || { echo >&2 "I require bash but it's not installed. Install it. Aborting."; exit 1; }
 command -v python2 > /dev/null 2>&1 || { echo >&2 "I require python2 but it's not installed. Install it. Aborting."; exit 1; }
 command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
+command -v openssl > /dev/null 2>&1 || { echo >&2 "I require openssl but it's not installed. Install it. Aborting."; exit 1; }
 
 }
 
@@ -49,14 +37,13 @@ printf "
         \e[0;2;96m▀▀█ █▀▀ █▀█ █ █ █▀▀ █    \e[0;93m █    █  █▀▀ █▀▀
         \e[0;2;96m▀▀▀ ▀   ▀ ▀ ▀ ▀ ▀▀▀ ▀    \e[0;93m ▀▀▀ ▀▀▀ ▀   ▀▀▀
 "
+
 }
 
 template() {
 
 echo
-printf "\e[0m[\e[94;1m#\e[0m] Various kinds of tools for spam\n"
-printf "\e[0m[\e[93;1m*\e[0m] Coded by Senja\n"
-printf "\e[0m[\e[96;1m&\e[0m] My Github: @thedarksec\n"
+printf "\e[0;46;90;1m:         Spamer Life v1.0, Coded by @stepbystep         :\e[0m\n"
 sleep 1
 echo
 
@@ -64,13 +51,16 @@ echo
 
 menu() {
 
-printf "\e[0m[\e[92;1m1\e[0m] \e[77;1mAlpha       \e[0m[\e[92;1m6\e[0m] \e[77;1mTaxi\n"
-printf "\e[0m[\e[92;1m2\e[0m] \e[77;1mBukalapak   \e[0m[\e[92;1m7\e[0m] \e[77;1mTelkomsel\n"
-printf "\e[0m[\e[92;1m3\e[0m] \e[77;1mGrab        \e[0m[\e[92;1m8\e[0m] \e[77;1mTokopedia\n"
-printf "\e[0m[\e[92;1m4\e[0m] \e[77;1mHooqy       \e[0m[\e[92;1m9\e[0m] \e[77;1mTri\n"
-printf "\e[0m[\e[92;1m5\e[0m] \e[77;1mOyoroom     \e[0m[\e[92;1m0\e[0m] \e[77;1mExit\n"
+printf "\e[0m[\e[96;1m1\e[0m] \e[77;1mAlpha         \e[0m[\e[96;1m6\e[0m] \e[77;1mHooqy       \e[0m[\e[96;1m6\e[0m] \e[77;1mTelkomsel\n"
+printf "\e[0m[\e[96;1m2\e[0m] \e[77;1mBukalapak     \e[0m[\e[96;1m7\e[0m] \e[77;1mOyoroom     \e[0m[\e[96;1m6\e[0m] \e[77;1mTokopedia\n"
+printf "\e[0m[\e[96;1m3\e[0m] \e[77;1mGrab          \e[0m[\e[96;1m8\e[0m] \e[77;1mTaxi        \e[0m[\e[96;1m6\e[0m] \e[77;1mTri\n"
+echo
+printf "\e[0m[\e[93;1m&\e[0m] \e[0mLICENSE\n"
+printf "\e[0m[\e[94;1m#\e[0m] \e[0mInformation\n"
+printf "\e[0m[\e[92;1m*\e[0m] \e[0mUpdate\n"
+printf "\e[0m[\e[91;1m-\e[0m] \e[0mExit\n"
 
-read -p $'\n\e[0m[\e[1;95m/\e[0m\e[0m] \e[1;77mSelect an option: \e[0m\en' option
+read -p $'\n\n\e[0m[\e[1;95m/\e[0m\e[0m] \e[1;77mSelect an option: \e[0m\en' option
 
 if [[ $option == 01 || $option == 1 ]]; then
 alpha
@@ -99,7 +89,25 @@ tokopedia
 elif [[ $option == 09 || $option == 9 ]]; then
 tri
 
-elif [[ $option == 00 || $option == 0 ]]; then
+elif [[ $option == '&' ]]; then
+echo
+nano LICENSE
+echo
+exit 1
+
+elif [[ $option == '#' ]]; then
+echo
+nano README.md
+echo
+exit 1
+
+elif [[ $option == '*' ]]; then
+echo
+git pull origin master
+echo
+exit 1
+
+elif [[ $option == '-' ]]; then
 echo
 printf "\e[0m[\e[1;91m!\e[0m] \e[1;77mExit the program!\n"
 echo
@@ -128,7 +136,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mAlpha\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mSMS\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mLimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPython\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -147,7 +155,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mBukalapak\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mChats WhatsApp\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mUnlimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPHP\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -166,7 +174,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mGrab\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mCall\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mUnlimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPHP\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -185,7 +193,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mHooqy\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mOTP\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mUnlimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPython\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -204,7 +212,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mOyoroom\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mSMS\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mLimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPython\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -223,7 +231,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mTaxi\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mCall\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mUnlimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPython\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -242,7 +250,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mTelkomsel\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mSMS\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mUnlimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPython\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -261,7 +269,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mTokopedia\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mCall\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mLimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPHP\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -280,7 +288,7 @@ printf "\e[0m[\e[1;92m+\e[0m] \e[77;1mSpamm: \e[0mTri\n"
 printf "\e[0m[\e[1;93m*\e[0m] \e[77;1mTypes: \e[0mSMS\n"
 printf "\e[0m[\e[1;94m#\e[0m] \e[77;1mCount: \e[0mUnlimited\n"
 printf "\e[0m[\e[1;95m@\e[0m] \e[77;1mLanguage: \e[0mPython\n"
-printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mSenja\n"
+printf "\e[0m[\e[1;96m&\e[0m] \e[77;1mCoded by: \e[0mNedi Senja\n"
 echo
 sleep 1.5
 echo
@@ -290,7 +298,6 @@ python2 tri.py
 }
 
 #checkroot
-#checktor
 #dependencies
 clearscreen
 banner
