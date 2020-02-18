9338b27bb716dca251146fb85f25d125e2b37af4
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-02-19 05:40:50 +0700

    Update alpha.py

--- tools/alpha.py
+++ tools/alpha.py
@@ -1,8 +1,8 @@
 import os, sys, time, mechanize
 from bs4 import BeautifulSoup as BS
+
 class spamer:
         def __init__(self):
-                #install browser
                 self.br = mechanize.Browser()
                 self.br.set_handle_equiv(True)
                 self.br.set_handle_gzip(True)
@@ -25,10 +25,12 @@ class spamer:
                 ]
                 self.url = 'http://sms.payuterus.biz/alpha/'
                 self.banner()
+
         def banner(self):
-                no = input('\033[0m[\033[0m\033[1;32m+\033[0m] \033[1;77mInput No Target: \033[0m')
-                msg = input('\033[0m[\033[0m\033[1;34m~\033[0m] \033[1;77mMesagee: \033[0m')
+                no = input('\033[0m[\033[0m\033[1;32m+\033[0m] \033[1;77mMasukan Nomor Target: \033[0m')
+                msg = input('\033[0m[\033[0m\033[1;34m~\033[0m] \033[1;77mPesan: \033[0m')
                 self.main(no,msg)
+
         def main(self,no,msg):
                 o = []
                 bs = BS(self.br.open(self.url),features = 'html.parser')
@@ -42,23 +44,25 @@ class spamer:
                 self.br.form['pesan']=msg
                 self.br.form['captcha']=str(capt)
                 sub = self.br.submit().read()
-                if '\033[0m[\033[0m\033[1;34m#\033[0m] \033[1;77mSucces to send message in the free \033[0m' in str(sub):
-                        print('\033[0m[\033[0m\033[1;36m/\033[0m] \033[1;77mSucces send message \033[0m',no)
-                elif '\033[0m[\033[0m\033[1;33m*\033[0m] Waiting in 8 second as backend \033[0m' in str(sub):
-                        print('\033[0m[\033[0m\033[1;31m!\033[0m] \033[1;77mPlease wait as the message')
+                if '\033[0m[\033[0m\033[1;34m#\033[0m] \033[1;77mBerhasil mengirim pesan \033[0m' in str(sub):
+                        print('\033[0m[\033[0m\033[1;36m/\033[0m] \033[1;77mSukses terkirim \033[0m',no)
+                elif '\033[0m[\033[0m\033[1;33m*\033[0m] Tunggu 8 detik untuk kembali \033[0m' in str(sub):
+                        print('\033[0m[\033[0m\033[1;31m!\033[0m] \033[1;77mMemproses pengiriman')
                 else:
-                        print('\033[0m[\033[0m\033[1;31m-\033[0m] \033[1;77mSend failed the \033[0m',no)
+                        print('\033[0m[\033[0m\033[1;31m-\033[0m] \033[1;77mMengirim gagal ke nomor \033[0m',no)
+             
 try:
         spamer()
         while True:
-                w = input("\n\033[0m[\033[0m\033[1;36m?\033[0m] \033[1;77mRestart program \033[0m[Y/n]: \033[1;77m")
+                w = input("\n\033[0m[\033[0m\033[1;36m?\033[0m] \033[1;77mMulai ulang \033[0m[Y/n]: \033[1;77m")
                 if w.lower() == 'Y' or 'y':
                         spamer()
                 elif w.lower() == 'N' or 'n':
-                        exit('\033[0m[\033[0m\033[1;31m!\033[0m] \033[1;77mExit the program')
+                        exit('\033[0m[\033[0m\033[1;31m!\033[0m] \033[1;77mKeluar dari program')
+
 except KeyboardInterrupt:
-        print('\n\033[0m[\033[0m\033[1;31m!\033[0m] \033[1;77mError: KeyboardInterrupt')
+        print('\n\033[0m[\033[0m\033[1;31m!\033[0m] \033[1;77mKata kunci sala')
 except Exception as p:
-        print('\033[0m[\033[0m\033[1;31m!\033[0m] \033[1;77mInvalid the interupt')
+        print('\033[0m[\033[0m\033[1;31m!\033[0m] \033[1;77mTidak sesuiai kata kunci')
         print
         sys.exit(1)
